85b08a7687c487cb6dbedd596c44cd246fbbe9a2
Colin Edwards <ColinJhEdwards@gmail.com>
2021-06-19 08:41:15 -0700

    styled navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBroom } from "@fortawesome/free-solid-svg-icons";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
 
 const NavC = () => {
   return (
@@ -10,18 +10,22 @@ const NavC = () => {
       <Navbar
         className="navEdit"
         collapseOnSelect
-        expand="lg"
+        expand="sm"
         // bg="dark"
         variant="dark"
       >
-        <Navbar.Brand href="#home">
-          Sweep <FontAwesomeIcon className="sweepIcon" icon={faBroom} />
+        <Navbar.Brand className="brand" href="#home">
+          Sweep <FontAwesomeIcon className="sweepIcon" icon={faFilm} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto justify-content-end">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav className="ml-auto">
+            <Nav.Link className="p" href="#features">
+              Home
+            </Nav.Link>
+            <Nav.Link className="p" href="#pricing">
+              Search
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
@@ -32,8 +36,22 @@ const NavC = () => {
 const StyledNav = styled.div`
   .navEdit {
     background: #0202a7;
+    .brand {
+      font-size: 2rem;
+    }
     .sweepIcon {
       color: #c07d00;
+    }
+    .ml-auto {
+      font-size: 2rem;
+      .p {
+        color: white;
+        margin: 0rem 2rem;
+        transition: all ease 0.5s;
+        &:hover {
+          color: #c07d00;
+        }
+      }
     }
   }
 `;
