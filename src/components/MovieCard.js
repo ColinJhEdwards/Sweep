717316b7f39aebc7cfb26e0426c3c0cdcbe40766
Colin Edwards <ColinJhEdwards@gmail.com>
2022-04-17 15:50:48 -0700

    changed style of movie card on home page

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -20,12 +20,14 @@ const MovieCard = ({ title, imagePath, release, id }) => {
   return (
     <Link to={`/movie/${id}`} onClick={() => loadMovieDetailHandler(id, title)}>
       <StyledCard>
+        <div className="hide">
+          <LazyLoadImage
+            src={`https://image.tmdb.org/t/p/original${imagePath}`}
+            alt="poster"
+            effect="blur"
+          />
+        </div>
         <h2>{title}</h2>
-        <LazyLoadImage
-          src={`https://image.tmdb.org/t/p/original${imagePath}`}
-          alt="poster"
-          effect="blur"
-        />
         <p>{release}</p>
       </StyledCard>
     </Link>
@@ -38,12 +40,16 @@ const StyledCard = styled(motion.div)`
   justify-content: center;
   align-items: center;
   box-shadow: 0px 0px 20px rgb(0, 0, 0);
-  border-radius: 15px;
-  padding: 1rem;
+  border-radius: 10px;
+  padding: 0rem;
+  position: relative;
   transition: all ease 0.5s;
   cursor: pointer;
   &:hover {
     box-shadow: 0px 0px 20px rgb(4, 0, 218);
+  }
+  .hide {
+    overflow: hidden;
   }
   h2 {
     text-align: center;
@@ -54,9 +60,10 @@ const StyledCard = styled(motion.div)`
     margin: 1rem 0rem;
   }
   img {
-    width: 200px;
+    width: 100%;
     box-shadow: 0px 0px 20px rgb(0, 0, 0);
-    border-radius: 10px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
   }
   @media (max-width: 1495px) {
     margin: 1rem;
